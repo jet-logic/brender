db5bd9b31c07ad22724aaf69e43d78f7aba3050b
jet-logic <https://github.com/jet-logic>
2025-09-01 01:38:41 +0800

    First

---
+++ src/brender/__init__.py
@@ -0,0 +1,329 @@
+from dataclasses import dataclass, field
+import inspect
+import shlex
+from math import ceil
+from subprocess import Popen
+from os import path, cpu_count
+from tempfile import NamedTemporaryFile, gettempdir
+from pathlib import Path
+from json import load
+from sys import stderr
+
+
+def say(*args, **kwargs):
+    print(*args, file=stderr, **kwargs)
+
+
+def ffargs(d: dict):
+    if isinstance(d, (list, tuple)):
+        for v in d:
+            yield str(v)
+    elif isinstance(d, str):
+        yield from shlex.split(d)
+    else:
+        for k, v in d.items():
+            if k:
+                if v is True:
+                    yield f"-{k}"
+                    continue
+                elif v is False:
+                    yield f"-no{k}"
+                    continue
+                else:
+                    yield f"-{k}"
+                    if v is not None:
+                        yield str(v)
+
+
+@dataclass
+class Render:
+    blender_file = "target.blend"
+    scene_name = "Scene"
+    skip_factor = 1
+    frame_range = (1, 100)
+    workers = min(cpu_count() / 4, 2)
+    res_percent = -1  # 0-100
+    with_audio: bool | None = None
+    blender_bin = "blender"
+    audio_suffix = ".wav"
+    frames_stem = "F######"
+    frames_strf = "F{!d}"
+    frames_format = "png"
+    ffmpeg_bin = "ffmpeg"
+    container: str = "mov"  # avi, mov, mkv, mp4
+    video_args: dict | list | str = "-c:v prores_ks -profile:v 5"
+    audio_args: dict | list | str = field(default_factory=lambda: [])
+    sound_mix_args = {"container": "WAV", "codec": "PCM"}
+    final_video: str = field(init=False)
+    video_path: str = field(init=False)
+    audio_path: str = field(init=False)
+    frames_dir: str = field(init=False)
+    final_fps: int = field(init=False)
+    file_info: dict = field(init=False)
+    has_audio: bool = field(init=False)
+    start_frame: int = field(init=False)
+    end_frame: int = field(init=False)
+    final_audio: str = field(init=False)
+    output_dir: str = field(init=False)
+    input_file: str = ""
+    delete_audio_after: bool | None = None
+    delete_frames_after: bool | None = None
+    _jobs: list[Popen] = field(default_factory=list)
+    prepare = None
+    ready = None
+
+    def __getattr__(self, name):
+        f = not name.startswith("_get_") and getattr(self, f"_get_{name}", None)
+        if f:
+            setattr(self, name, None)
+            v = f()
+            setattr(self, name, v)
+            return v
+        try:
+            m = super().__getattr__
+        except AttributeError:
+            pass
+        else:
+            return m(name)
+        c = self.__class__
+        raise AttributeError(
+            f"{c.__module__}.{c.__qualname__} has no attribute '{name}'"
+        )
+
+    def _get_output_dir(self):
+        return Path(gettempdir()) / (
+            Path(self.blender_file).stem + "_" + self.scene_name
+        )
+
+    def _get_frames_dir(self):
+        return str(
+            Path(gettempdir()) / (Path(self.blender_file).stem + "_" + self.scene_name)
+        )
+
+    def _get_final_audio(self):
+        return str(
+            Path(gettempdir())
+            / (
+                Path(self.blender_file).stem
+                + "_"
+                + self.scene_name
+                + "."
+                + self.sound_mix_args["container"].lower()
+            )
+        )
+
+    def _get_final_video(self):
+        return str(
+            Path(gettempdir())
+            / (
+                Path(self.blender_file).stem
+                + "_"
+                + self.scene_name
+                + f".{self.container.lower()}"
+            )
+        )
+
+    def _get_has_audio(self):
+        return self.file_info["has_audio"]
+
+    def _get_final_fps(self):
+        return (self.file_info["fps"] // self.skip_factor) / self.file_info["fps_base"]
+
+    def _get_start_frame(self):
+        return self.file_info["frame_range"][0]
+
+    def _get_end_frame(self):
+        return self.file_info["frame_range"][1]
+
+    def _get_file_info(self):
+        with NamedTemporaryFile(delete_on_close=False) as msg:
+            msg.close()
+            Popen(
+                [
+                    self.blender_bin,
+                    "-b",
+                    self.blender_file,
+                    "-S",
+                    self.scene_name,
+                    "-q",
+                    "--python-expr",
+                    (
+                        "import bpy, sys, os, json;"
+                        "from subprocess import Popen;"
+                        "from pathlib import Path;"
+                        "scene=bpy.data.scenes['%s']; "
+                        "Path(%r).write_text(json.dumps({"
+                        "'frame_range':[scene.frame_start,scene.frame_end],"
+                        "'fps':scene.render.fps,"
+                        "'fps_base':scene.render.fps_base,"
+                        "'has_audio':any(seq.type=='SOUND' for seq in getattr(scene.sequence_editor,'sequences_all',[]))"
+                        "}))"
+                    )
+                    % (self.scene_name, msg.name),
+                ],
+                stdout=None,
+            ).wait()
+            with open(msg.name) as r:
+                return load(r)
+
+    def render_frames(self, **kwargs):
+        start_frame = self.start_frame
+        end_frame = self.end_frame
+        skip_factor = self.skip_factor
+        workers = self.workers
+        scene_name = self.scene_name
+        blender_bin = self.blender_bin
+        frame_count = ((end_frame - start_frame) // skip_factor) + 1
+        chunk_size = ceil(frame_count / workers)
+        frames_dir = self.frames_dir
+        frames_stem = self.frames_stem
+        frames_format = self.frames_format
+        output_path = path.join(frames_dir, f"{frames_stem}.{frames_format}")
+
+        # --- start render chunks ---
+        func = None
+        try:
+            from __main__ import prepare as func
+        except ImportError:
+            pass
+
+        head = [
+            blender_bin,
+            "-b",
+            self.blender_file,
+            "-S",
+            scene_name,
+            "-o",
+            output_path,
+        ]
+        tail = ["-F", frames_format.upper()]
+        if skip_factor != 1:
+            tail.extend(["-j", (skip_factor)])
+        tail += [
+            "--python-expr",
+            ";".join(
+                [
+                    v
+                    for v in (
+                        f"import bpy",
+                        f"scene = bpy.data.scenes['{scene_name}']",
+                        (
+                            f"scene.render.resolution_percentage={self.res_percent};"
+                            if self.res_percent > 0
+                            else ""
+                        ),
+                    )
+                    if v
+                ]
+            )
+            + (
+                ("\n" + inspect.getsource(func) + "\n" + f"{func.__name__}(scene)")
+                if func
+                else ""
+            ),
+        ]
+
+        tail += ["-a"]
+        for i in range(workers):
+            s = start_frame + i * chunk_size * skip_factor
+            e = min(end_frame, s + (chunk_size - 1) * skip_factor)
+            if s > end_frame:
+                break
+            cmd = [*head, "-s", s, "-e", e, *tail]
+            say("Frames:", *cmd)
+            self._jobs.append(Popen([str(x) for x in cmd]))
+        return self
+
+    def mix_sound(self):
+        mix_args = {"filepath": self.final_audio, "container": "WAV", "codec": "PCM"}
+        cmd = [
+            self.blender_bin,
+            "-b",
+            self.blender_file,
+            "-S",
+            self.scene_name,
+            "--python-expr",
+            f"import bpy; kwa={mix_args!r}; bpy.ops.sound.mixdown(**kwa)",
+        ]
+        say("Audio:", *cmd)
+        self._jobs.append(Popen(cmd))
+        return self
+
+    def frame_files(self, check_render=False):
+        start_frame = self.start_frame
+        end_frame = self.end_frame
+        skip_factor = self.skip_factor
+        frames_dir = self.frames_dir
+        frames_format = self.frames_format
+        if check_render:
+            last = path.join(frames_dir, f"F%06d.{frames_format}" % (start_frame))
+            if not path.exists(last):
+                self.render_frames().wait()
+        p = self.input_file = path.join(frames_dir, f"input.txt")
+        i = start_frame
+        e = end_frame
+        with open(p, "w") as w:
+            while i <= e:
+                w.write(f"file 'F%06d.{frames_format}'\n" % i)
+                i += skip_factor
+
+    def render_video(self, **kwargs):
+        cmd = [self.ffmpeg_bin]
+        cmd.extend(
+            ffargs(
+                {
+                    "r": f"{self.final_fps:f}".strip("0").strip("."),
+                    "hide_banner": True,
+                    "y": True,
+                    "v": "warning",
+                }
+            )
+        )
+        self.wait()
+        self.frame_files(check_render=True)
+
+        cmd += [
+            "-use_wallclock_as_timestamps",
+            "1",
+            "-f",
+            "concat",
+            "-protocol_whitelist",
+            "file",
+            "-safe",
+            "0",
+            "-i",
+            self.input_file,
+        ]
+
+        if self.with_audio is not False and self.has_audio:
+            if not path.exists(self.final_audio):
+                self.mix_sound().wait()
+            cmd += ["-i", self.final_audio]
+            cmd += ffargs(self.audio_args)
+            # cmd += ["-af", "apad=whole_dur=1"]
+
+        cmd.extend(ffargs(self.video_args))
+        cmd += [self.final_video]
+
+        say("Video:", shlex.join([str(x) for x in cmd]))
+        self._jobs.append(Popen([str(x) for x in cmd]))
+
+        return self
+
+    def wait(self):
+        jobs = self._jobs
+        while jobs:
+            jobs.pop().wait()
+
+    def debug(self):
+        from __main__ import prepare
+
+        source = inspect.getsource(prepare)
+        from sys import argv
+
+        print(prepare.__name__)
+
+        print(source, list(argv))
+
+
+# TODO: audio codec
